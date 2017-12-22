be0d7179b2f8bd084cc018fd0eb8cd7bc4eab31a
Marc Farra <marcfarra@gmail.com>
2017-12-22 13:24:05 -0500

    Add more stats

--- src/main/scala/StatsJob.scala
+++ src/main/scala/StatsJob.scala
@@ -19,9 +19,41 @@ object StatsJob {
         val roadsOnly: RDD[(Long, osm.Way)] =
           ws.filter(_._2.meta.tags.contains("highway"))
 
+        def newer(e1: osm.Way, e2: osm.Way): osm.Way =
+          if (e1.meta.version > e2.meta.version) e1 else e2
+
+        val roadsBeforeTimestamp = roadsOnly.filter(_._2.meta.timestamp.isBefore(Instant.parse("2017-11-01T00:00:00.00Z")))
         val roadsAfterTimestamp = roadsOnly.filter(_._2.meta.timestamp.isAfter(Instant.parse("2017-11-01T00:00:00.00Z")))
-        println(roadsAfterTimestamp.count())
-      }  /* (RDD[(Long, Node)], RDD[(Long, Way)], RDD[(Long, Relation)]) */
+
+        val latestRoadBeforeUpdate = roadsBeforeTimestamp.reduceByKey(newer)
+        val latestRoadAfterUpdate = roadsOnly.reduceByKey(newer)
+
+        /** Roads Created after timestamp */
+        val roadsCreated = roadsAfterTimestamp.filter(_._2.meta.version == 1L)
+        val roadsCreatedCount = roadsCreated.count()
+        println(s"Roads created: $roadsCreatedCount")
+
+        /** Roads deleted */
+        val visibleRoads = roadsAfterTimestamp.filter(_._2.meta.visible.equals(true))
+        val roadsDeleted = roadsAfterTimestamp.filter(_._2.meta.visible.equals(false))
+        val roadsDeletedCount = roadsDeleted.count()
+        println(s"Roads deleted: $roadsDeletedCount")
+
+        /** Roads modified */
+        val roadsModified = visibleRoads.filter(_._2.meta.version > 1L)
+        val roadsModifiedCount = roadsModified.count()
+        println(s"Roads modified: $roadsModifiedCount")
+
+        /** Calculating what changes occurred before/after timestamp*/
+        /*
+        def tagChanges(e1: osm.Way, e2: osm.Way): Map[String, Long] =
+          (e1.meta.tags.toSet diff e2.meta.tags.toSet)
+
+        latestRoadAfterUpdate.union(latestRoadBeforeUpdate)
+
+        println(s"Road counts: before: $roadCount1, after: $roadCount2")
+        */
+      }
     }
     ss.stop()
   }
